852ad69e92d665bfc17be3ebcb56b9ed33e9613b
kirtanuce23109 <kirtan.uce23109@cept.ac.in>
2026-04-13 11:51:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ import tempfile
 import pandas as pd
 import matplotlib.pyplot as plt
 from ultralytics import YOLO
+import os
 
 st.title("👷 PPE Detection Dashboard")
+
+# Check model
+MODEL_PATH = "best.pt"
+
+if not os.path.exists(MODEL_PATH):
+    st.error("❌ best.pt not found in GitHub repo")
+    st.stop()
 
 uploaded_file = st.file_uploader("Upload Video", type=["mp4"])
 
@@ -14,16 +22,16 @@ if uploaded_file is not None:
     tfile = tempfile.NamedTemporaryFile(delete=False)
     tfile.write(uploaded_file.read())
 
-    model = YOLO("best.pt")
+    model = YOLO(MODEL_PATH)
 
     cap = cv2.VideoCapture(tfile.name)
     stframe = st.empty()
 
-    workers = {"helmet":0, "no_helmet":0, "vest":0, "no_vest":0}
+    stats = {"helmet":0, "vest":0}
 
     while cap.isOpened():
         ret, frame = cap.read()
-        if not ret:
+        if not ret or frame is None:
             break
 
         results = model(frame)
@@ -33,16 +41,16 @@ if uploaded_file is not None:
                 cls = int(box.cls[0])
 
                 if cls == 1:
-                    workers["helmet"] += 1
+                    stats["helmet"] += 1
                 elif cls == 2:
-                    workers["vest"] += 1
+                    stats["vest"] += 1
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         stframe.image(frame)
 
     cap.release()
 
-    df = pd.DataFrame([workers])
+    df = pd.DataFrame([stats])
 
     st.subheader("📊 PPE Stats")
     st.dataframe(df)
